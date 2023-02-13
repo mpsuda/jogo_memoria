d227b8ed20ae774d132a7a981691e34749a578c9
Marcos Suda <marcos.suda@gmail.com>
2023-02-13 18:11:27 -0300

    Inclusao de placar de pontos, botoes de reinicio e limpar

--- src/js/index.js
+++ src/js/index.js
@@ -19,15 +19,28 @@ var placar = 0;
 var tempo = 0;
 
 var intervalo = setInterval(function () {
-  if (tempo == 30) {
+  if (tempo == 60 || placar == 8) {
     clearInterval(intervalo);
-    alert("Tempo esgotado!");
-    var nome = prompt("Digite o seu nome:");
-    //Criar um objeto
-    var resultado = {
-      nomeJogador: nome,
-      pontuacao: placar,
-    };
+    if (placar == 8) {
+        alert(
+          "Parabéns! Você ganhou! O seu tempo foi de " + tempo + " segundos."
+        );
+    } else {
+        alert("Tempo esgotado!");
+      }    
+      var nome = "";
+      while (nome.length < 3) {
+        nome = prompt("Digite o seu nome:");
+        if (nome.length < 3) {
+          alert("O nome precisa ter mais que 3 caracteres");
+        }
+      }
+      
+      var resultado = {
+        nomeJogador: nome,
+        pontuacao: placar,
+        tempo: tempo,
+      };
 
     resultadosExistentes.push(resultado);
     localStorage.setItem("resultado", JSON.stringify(resultadosExistentes));
@@ -89,6 +102,37 @@ function virarCartao() {
     }
 }
 
-cartoes.forEach(function(cartao) {
-    cartao.addEventListener("click",virarCartao);
-});
+cartoes.forEach(function (cartao) {
+    var numero = Math.floor(Math.random() * 16);
+    cartao.style.order = numero;
+  });
+  
+  cartoes.forEach(function (cartao) {
+    cartao.addEventListener("click", virarCartao);
+  });
+
+  function ordenarDados() {
+    var melhorTempo = resultadosExistentes[0].tempo;
+    resultadosExistentes.forEach(function (resultado) {
+      if (resultado.tempo < melhorTempo) {
+        resultadosExistentes.push(melhorTempo);
+      }
+    });
+  }
+
+  function reiniciarPlacar() {
+    console.log(resultadosExistentes.length);
+    for (var i = 2; i < resultadosExistentes.length + 2; i++){
+      var resultado = document.querySelector('#resultados > tr:nth-child(2)');
+      if (resultado) {
+      document.querySelector("#resultados").removeChild(resultado);
+      }
+    }
+
+    localStorage.clear();
+    alert("Placar reiniciado!");    
+  }
+
+  function reiniciarJogo() {
+    location.reload();
+  }
